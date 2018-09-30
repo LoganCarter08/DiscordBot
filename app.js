be9d89b1474da8d9f8f902a2bd1bed9899d5fb95
Logan <loganvt8@vt.edu>
2018-09-30 03:21:40 +0000

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-var forever = require('forever');
-var child = new (forever.Monitor )('bot.js', {
-  //options : options
-} );
-
-//These lines actually kicks things off
-child.start();
-forever.startServer( child );
